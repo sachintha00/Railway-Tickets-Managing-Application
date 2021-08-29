a0e684fedee5f6f2601fc97816850abdfaeba4a1
sachintha00 <sachintha0603@gmail.com>
2021-08-29 20:25:12 +0530

    updated

--- RailwayManagementSystem.java
+++ RailwayManagementSystem.java
@@ -3,12 +3,6 @@ import java.util.HashMap;
 
 public class RailwayManagementSystem{
     public static void main(String[] args) {
-        List<RailwayTable> railwayData = new ArrayList<>();
-
-        railwayData.add(new RailwayTable("Matara", 1, 100.00));
-        railwayData.add(new RailwayTable("Ambalangoda", 2, 150.00));
-        railwayData.add(new RailwayTable("Kaluthara", 3, 200.00));
-        railwayData.add(new RailwayTable("Colombo", 4, 300.00));
 
         // Map<Integer,String> map = new HashMap<>();
         // for(RailwayTable data: railwayData){
