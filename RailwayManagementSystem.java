6b25f6d6396e925aaabd92eb1a145a050c2a153d
sachintha00 <sachintha0603@gmail.com>
2021-08-29 20:20:47 +0530

    updated

--- RailwayManagementSystem.java
+++ RailwayManagementSystem.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class RailwayManagementSystem{
     public static void main(String[] args) {
-        System.out.println("sahintha madhawa");
-        Ticket obj = new Ticket();
-        obj.setDesignation(32);
-        System.out.println(obj.getDesignation());
+        List<RailwayTable> railwayData = new ArrayList<>();
+
+        railwayData.add(new RailwayTable("Matara", 1, 100.00));
+        railwayData.add(new RailwayTable("Ambalangoda", 2, 150.00));
+        railwayData.add(new RailwayTable("Kaluthara", 3, 200.00));
+        railwayData.add(new RailwayTable("Colombo", 4, 300.00));
+
+        // Map<Integer,String> map = new HashMap<>();
+        // for(RailwayTable data: railwayData){
+        //     // map.put()
+        //     System.out.println(data.getDesignation());
+        // }
     }
 }
