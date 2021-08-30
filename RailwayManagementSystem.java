c4de647a402b3dc2c1036b41f87df89307421366
sachintha00 <sachintha0603@gmail.com>
2021-08-30 12:24:11 +0530

    Update RailwayManagementSystem.java

--- RailwayManagementSystem.java
+++ RailwayManagementSystem.java
@@ -1,13 +1,7 @@
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class RailwayManagementSystem{
     public static void main(String[] args) {
 
-        // Map<Integer,String> map = new HashMap<>();
-        // for(RailwayTable data: railwayData){
-        //     // map.put()
-        //     System.out.println(data.getDesignation());
-        // }
+        
     }
 }
