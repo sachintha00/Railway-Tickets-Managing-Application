6050c6f6eb787e03f8a536439f476c37ec91011a
sachintha00 <sachintha0603@gmail.com>
2021-08-30 14:19:21 +0530

    updated

--- RailwayManagementSystem.java
+++ RailwayManagementSystem.java
@@ -7,11 +7,29 @@ public class RailwayManagementSystem{
         list.add(new RailwayDataTable("Matara",1,100.00));
         list.add(new RailwayDataTable("Matara",2,200.00));
         list.add(new RailwayDataTable("Matara",3,300.00));
+        dataTaleDesign(list);
+    }
 
-        for(RailwayDataTable data : list){
-            if(data.getId() == 1){
-                System.out.println((double)data.getPrice()*2);
-            }
+    public static void dataTaleDesign(List<RailwayDataTable> list){
+        List<RailwayDataTable> list2 =list;
+        // List<RailwayDataTable> list = new ArrayList<>();
+        // list.add(new RailwayDataTable("Matara",1,100.00));
+        // list.add(new RailwayDataTable("Matara",2,200.00));
+        // list.add(new RailwayDataTable("Matara",3,300.00));
+
+
+
+        System.out.println("----------------------------------------------------------------------------");
+        System.out.print("\tDestination\t\t");
+        System.out.print("Destination ID\t\t");
+        System.out.println("Ticket Price");
+        System.out.println("----------------------------------------------------------------------------");
+
+        for(RailwayDataTable data : list2){
+            System.out.print("\t"+data.getDesignation()+"\t\t\t\t");
+            System.out.print(data.getId()+"\t\t");
+            System.out.println(" "+data.getPrice());
         }
+        System.out.println("----------------------------------------------------------------------------");
     }
 }
